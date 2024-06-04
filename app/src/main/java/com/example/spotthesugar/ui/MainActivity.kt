d62d68ec50ae85aa55c6a0aa3bec944a59d945a1
Wonder11ust <matthew0379@gmail.com>
2024-06-04 22:05:11 +0700

    first commit

---
+++ app/src/main/java/com/example/spotthesugar/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.spotthesugar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spotthesugar.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
