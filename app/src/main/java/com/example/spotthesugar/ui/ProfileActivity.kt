b1d63a6bb3570b9b974be765ff1b98ae58213fca
Wonder11ust <matthew0379@gmail.com>
2024-06-05 23:39:31 +0700

    feature update: profileUI,LoginActivity,ResultBarcodeUI,SignUpUI

---
+++ app/src/main/java/com/example/spotthesugar/ui/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.spotthesugar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spotthesugar.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
